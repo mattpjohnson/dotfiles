3de0032ce84ef09ce731cdca7af8a4afa59945e6
Matt Johnson <matt@mattpjohnson.com>
2019-05-18 10:31:16 -0700

    feat(setup): Install tpm

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
-// Install vim-plug
+# Install vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# Install tpm
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
